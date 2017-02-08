364daebc38b22280239ba9bfb111996d919289c0
yafim <ifim.vo@gmail.com>
2017-02-08 20:36:51 +0200

    upload

---
+++ CoAppAssignment/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace CoAppAssignment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxTableName = new System.Windows.Forms.TextBox();
+            this.btnShowTree = new System.Windows.Forms.Button();
+            this.errLabel = new System.Windows.Forms.Label();
+            this.textBoxNumberOfValuesPerKey = new System.Windows.Forms.TextBox();
+            this.labelNumberOfValuesPerKey = new System.Windows.Forms.Label();
+            this.labelTableName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxTableName
+            // 
+            this.textBoxTableName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxTableName.Location = new System.Drawing.Point(687, 43);
+            this.textBoxTableName.Name = "textBoxTableName";
+            this.textBoxTableName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxTableName.TabIndex = 0;
+            // 
+            // btnShowTree
+            // 
+            this.btnShowTree.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnShowTree.Location = new System.Drawing.Point(687, 69);
+            this.btnShowTree.Name = "btnShowTree";
+            this.btnShowTree.Size = new System.Drawing.Size(100, 23);
+            this.btnShowTree.TabIndex = 1;
+            this.btnShowTree.Text = "Show Tree";
+            this.btnShowTree.UseVisualStyleBackColor = true;
+            this.btnShowTree.Click += new System.EventHandler(this.btnShowTree_Click);
+            // 
+            // errLabel
+            // 
+            this.errLabel.AutoSize = true;
+            this.errLabel.Location = new System.Drawing.Point(613, 26);
+            this.errLabel.Name = "errLabel";
+            this.errLabel.Size = new System.Drawing.Size(0, 13);
+            this.errLabel.TabIndex = 2;
+            // 
+            // textBoxNumberOfValuesPerKey
+            // 
+            this.textBoxNumberOfValuesPerKey.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxNumberOfValuesPerKey.Location = new System.Drawing.Point(686, 20);
+            this.textBoxNumberOfValuesPerKey.Name = "textBoxNumberOfValuesPerKey";
+            this.textBoxNumberOfValuesPerKey.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNumberOfValuesPerKey.TabIndex = 3;
+            this.textBoxNumberOfValuesPerKey.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxNumberOfValuesPerKey_KeyPress);
+            // 
+            // labelNumberOfValuesPerKey
+            // 
+            this.labelNumberOfValuesPerKey.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelNumberOfValuesPerKey.AutoSize = true;
+            this.labelNumberOfValuesPerKey.Location = new System.Drawing.Point(492, 23);
+            this.labelNumberOfValuesPerKey.Name = "labelNumberOfValuesPerKey";
+            this.labelNumberOfValuesPerKey.Size = new System.Drawing.Size(189, 13);
+            this.labelNumberOfValuesPerKey.TabIndex = 4;
+            this.labelNumberOfValuesPerKey.Text = "Number Of Values Per Key (default 10)";
+            // 
+            // labelTableName
+            // 
+            this.labelTableName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelTableName.AutoSize = true;
+            this.labelTableName.Location = new System.Drawing.Point(613, 46);
+            this.labelTableName.Name = "labelTableName";
+            this.labelTableName.Size = new System.Drawing.Size(65, 13);
+            this.labelTableName.TabIndex = 5;
+            this.labelTableName.Text = "Table Name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(799, 498);
+            this.Controls.Add(this.labelTableName);
+            this.Controls.Add(this.labelNumberOfValuesPerKey);
+            this.Controls.Add(this.textBoxNumberOfValuesPerKey);
+            this.Controls.Add(this.errLabel);
+            this.Controls.Add(this.btnShowTree);
+            this.Controls.Add(this.textBoxTableName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxTableName;
+        private System.Windows.Forms.Button btnShowTree;
+        private System.Windows.Forms.Label errLabel;
+        private System.Windows.Forms.TextBox textBoxNumberOfValuesPerKey;
+        private System.Windows.Forms.Label labelNumberOfValuesPerKey;
+        private System.Windows.Forms.Label labelTableName;
+    }
+}
+
---
+++ Utils/Utils.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace Utils
+{
+    public class Utils
+    {
+        #region singleton
+
+        private static volatile Utils m_Instance;
+        private static object m_Locker = new Object();
+
+        private Utils()
+        {
+        }
+
+        public static Utils Instance
+        {
+            get
+            {
+                if (m_Instance == null)
+                {
+                    lock (m_Locker)
+                    {
+                        if (m_Instance == null)
+                            m_Instance = new Utils();
+                    }
+                }
+
+                return m_Instance;
+            }
+        }
+
+        #endregion
+
+        #region SQL Table inforamtion
+        private int m_UniqueIdCounter;
+        private string m_dbName = "AdventureWorks";
+        private string m_DataSource = "local";
+        #endregion
+
+        #region Getters And Setters
+
+        private string m_NoDataString = "--No Data--";
+        public string NoDataString { get { return m_NoDataString; } }
+        
+        #endregion
+
+        /// <summary>
+        /// Connect to local SQL with given database name and get its column names
+        /// </summary>
+        /// <param name="database">Database name</param>
+        /// <returns>Column names</returns>
+        public LinkedList<string> GetColumnNamesFromDB(string database)
+        {
+            LinkedList<string> columns = new LinkedList<string>();
+
+            SqlConnection connection = new SqlConnection();
+            SqlCommand cmd = new SqlCommand();
+
+
+            //Open a connection to the SQL Server Northwind database.
+            connection.ConnectionString = String.Format(
+                "Data Source=({0});Initial Catalog={1};Integrated Security=SSPI", m_DataSource, m_dbName);
+            try
+            {
+                connection.Open();
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Database Name Not Found");
+            }
+
+            cmd.Connection = connection;
+            cmd.CommandText = "SELECT * FROM " + database;
+            SqlDataReader reader = cmd.ExecuteReader(CommandBehavior.KeyInfo);
+
+            //Retrieve column schema into a DataTable.
+            var tableSchema = reader.GetSchemaTable();
+
+            foreach (DataRow row in tableSchema.Rows)
+            {
+                var name = row["ColumnName"].ToString();
+                columns.AddLast(new LinkedListNode<string>(name));
+
+            }
+
+            // Close
+            reader.Close();
+            connection.Close();
+
+            return columns;
+        }
+
+
+
+        /// <summary>
+        /// Get rows from table by columns
+        /// </summary>
+        /// <param name="columns">Table Columns</param>
+        /// <param name="database">Table name</param>
+        /// <returns>Dictionary<string, LinkedList<string>> s.t key is the column</returns>
+        public Dictionary<string, LinkedList<string>> GetValuesFromTableByColumns(LinkedList<string> columns,
+            string database)
+        {
+            Dictionary<string, LinkedList<string>> dictionary = new Dictionary<string, LinkedList<string>>();
+
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = String.Format(
+                "Data Source=({0});Initial Catalog={1};Integrated Security=SSPI", m_DataSource, m_dbName);
+            connection.Open();
+
+
+            // get values
+            foreach (var col in columns)
+            {
+                // Create new DataAdapter
+                using (SqlDataAdapter adapter = new SqlDataAdapter(
+                    "SELECT " + col + " FROM " + database, connection))
+                {
+                    // fill DataTable
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+
+                    // Render data onto the screen
+                    LinkedList<string> list = new LinkedList<string>();
+
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        string val = row[col].ToString();
+                        list.AddFirst(val);
+                    }
+
+                    dictionary.Add(col, list);
+                }
+
+            }
+
+            connection.Close();
+
+            return dictionary;
+        }
+
+        /// <summary>
+        /// Generate unique id for nodes without actual value inside.
+        /// </summary>
+        /// <returns>uniqueid</returns>
+        public string GenerateUniqueId()
+        {
+            m_UniqueIdCounter++;
+            return String.Format(@"uniqueId_{0}", m_UniqueIdCounter);
+        }
+
+    }
+}
